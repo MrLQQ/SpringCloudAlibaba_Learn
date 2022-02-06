4423357993681a6a6ad46ab8300f0938629a9931
MrLQQ <2802351094@qq.com>
2022-02-06 23:43:15 +0800

    SpringCloud Config分布式配置中心

---
+++ cloud-config-center-3344/src/main/java/com/mrlqq/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.mrlqq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Classname ConfigCenterMain3344
+ * @Description TODO
+ * @Date 2022/2/6 17:26
+ * @Created by LQQ
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/mrlqq/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,17 @@
+package com.mrlqq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname ConfigClientMain3355
+ * @Description TODO
+ * @Date 2022/2/6 18:14
+ * @Created by LQQ
+ */
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class,args);
+    }
+}
