277632a6d793ca87b4b0ebb8fad2ce455aeac369
MrLQQ <2802351094@qq.com>
2022-02-09 00:26:43 +0800

    Nacos服务配置中心，分类配置

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/mrlqq/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,21 @@
+package com.mrlqq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Classname NacosConfigClientMain3377
+ * @Description TODO
+ * @Date 2022/2/8 14:24
+ * @Created by LQQ
+ */
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377
+{
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
